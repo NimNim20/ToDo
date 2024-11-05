f3dd548df9b9928343c24e4b9be74bd3aa981b1f
Benjamin Vid Gard <benvio01@easv365.dk>
2024-11-05 20:08:45 +0100

    Testing the test again

--- tests/toDo-tests.js
+++ tests/toDo-tests.js
@@ -1,11 +1,30 @@
-// import { Selector } from 'testcafe';
+import { Selector } from 'testcafe';
 
-// fixture `ToDo`
-//     .page `https://test.bimmersen365.dk/todo/`;
+// Testing if you can add a task to the list
+fixture ("Adding a task to the list")
+    .page("https://test.bimmersen365.dk/todo/");
 
-// test('Add new task', async t => {
-//     await t
-//         .typeText('#task', 'New Task')
-//         .click('#addTask')
-//         .expect(Selector('#tasks').innerText).contains('New Task');
-// });
+test("Adding a task", async t => {
+    await t 
+    .typeText('#todo-input' , 'Test task')
+    .click ('#addbutton')
+    .expect(Selector('li').innerText).contains('Test task')
+})
+
+
+test ("Editing a todo", async t => {
+await t
+.setNativeDialogHandler((type, text) => {
+    switch (type) {
+        case 'prompt':
+            return 'Edited todo';
+    }
+})
+
+.typeText('#todo-input' , 'Test task')
+.click ('#addbutton')
+.expect(Selector('li').innerText).contains('Test task')
+.click('#editBtn')
+.expect(Selector('li').innerText).contains('Edited todo');
+
+});
