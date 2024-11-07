358c876c1887887ffb1dac7458113e8ec2fa09ec
Benjamin Vid Gard <benvio01@easv365.dk>
2024-11-07 17:25:38 +0100

    Fixed missing bracket

--- tests/toDo-tests.js
+++ tests/toDo-tests.js
@@ -1,43 +1,37 @@
 import { Selector } from 'testcafe';
 
-fixture ("Adding a task to the list")
+fixture("Todo App Tests")
     .page("https://test.bimmersen365.dk/todo/");
 
 test("Adding a task", async t => {
     await t 
-    .typeText('#todo-input' , 'Do the dishes')
-    .click ('#add-todo')
-    .expect(Selector('li').innerText).contains('Do the dishes')
-})
+        .typeText('#todo-input', 'Do the dishes')
+        .click('#add-todo')
+        .expect(Selector('li').innerText).contains('Do the dishes');
+});
 
+test("Editing a todo", async t => {
+    await t
+        .setNativeDialogHandler((type, text) => {
+            if (type === 'prompt') {
+                return 'Edited todo';
+            }  
+        })
+        .typeText('#todo-input', 'Help with homework')
+        .click('#add-todo')
+        .expect(Selector('li').innerText).contains('Help with homework')
+        .click('#edit')
+        .expect(Selector('li').innerText).contains('Edited todo');
+});
 
-test ("Editing a todo", async t => {
-await t
-.setNativeDialogHandler((type, text) => {
-    switch (type) {
-        case 'prompt':
-            return 'Edited todo';
-    }  
-})
-
-
-    .typeText('#todo-input' , 'Help with homework')
-    .click ('#add-todo')
-    .expect(Selector('li').innerText).contains('Help with homework')
-    .click('#edit')
-    .expect(Selector('li').innerText).contains('Edited todo');
-
-
-test ("Toggling dark mode", async t => {
-    const darkMode = Selector('#theme-toggle-button')
+test("Toggling dark mode", async t => {
+    const themeToggleButton = Selector('#theme-toggle-button');
 
     await t
-    .click('darkMode')
-    .expect(darkMode.hasClass('dark-mode')).ok('Dark mode is enabled')
+        .click(themeToggleButton)
+        .expect(themeToggleButton.hasClass('dark-mode')).ok('Dark mode should be activated');
 
     await t
-    .click('darkMode')
-    .expect(darkMode.hasClass('dark-mode')).notOk('Dark mode is disabled')
-})
-
+        .click(themeToggleButton)
+        .expect(themeToggleButton.hasClass('dark-mode')).notOk('Dark mode should be deactivated');
 });
