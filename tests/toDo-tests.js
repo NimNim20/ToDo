b78ab55b289432ca68fa3a42a6a98475af2fb448
Benjamin Vid Gard <benvio01@easv365.dk>
2024-11-07 19:34:34 +0100

    Even more testing

--- tests/toDo-tests.js
+++ tests/toDo-tests.js
@@ -37,22 +37,23 @@ test("Toggling dark mode", async t => {
 test("Progress bar updates correctly", async t => {
     const progressBar = Selector('#todo-progress-bar');
     const progressText = Selector('#progress-text');
-
+  
     // Check initial state of the progress bar
     await t
-    .expect(progressBar.getAttribute('value')).eql('0', 'Progress bar should start at 0%')
-    .expect(progressText.innerText).eql('0%', 'Progress text should start at 0%');
-
+      .expect(progressBar.getAttribute('value')).eql('0', 'Progress bar should start at 0%')
+      .expect(progressText.innerText).eql('0%', 'Progress text should start at 0%');
+  
     // Add and complete the first task, then verify progress
     await t
-    .typeText('#todo-input', 'Do the dishes')
-    .click('#add-todo')
-    .click(Selector('li').withText('Do the dishes').find('input[type="checkbox"]'))
-
-      // Explicitly call updateProgressBar to ensure it's recalculated
-      .eval(() => updateProgressBar()) // Force the progress bar update immediately
-
-      // Wait for the progress bar to update using TestCafe's waiting mechanism
-    .expect(progressBar.getAttribute('value')).eql('25', 'Progress bar should update to 25% after first task')
-    .expect(progressText.innerText).eql('25%', 'Progress text should show 25%');
-});
+      .typeText('#todo-input', 'Do the dishes')
+      .click('#add-todo')
+      .click(Selector('li').withText('Do the dishes').find('input[type="checkbox"]'))
+      
+      // Force progress bar update
+      .eval(() => { updateProgressBar(); return true; })
+  
+      // Wait for the progress bar to update and assert its value
+      .expect(progressBar.getAttribute('value')).eql('25', 'Progress bar should update to 25% after first task')
+      .expect(progressText.innerText).eql('25%', 'Progress text should show 25%');
+  });
+  
